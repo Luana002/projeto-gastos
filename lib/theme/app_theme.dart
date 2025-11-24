f84a3f717935945a1e2d38649a9f3d03a24e45d5
Luana de Oliveira Corrêa <142100737+Luana002@users.noreply.github.com>
2025-11-24 02:54:26 +0000

    Colocando cores no app

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,14 +3,31 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       useMaterial3: true,
-      scaffoldBackgroundColor: Colors.grey[100],
+      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88E5)),
+      scaffoldBackgroundColor: const Color.fromARGB(255, 233, 243, 255),
       appBarTheme: const AppBarTheme(
-        elevation: 1,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(255, 13, 33, 12),
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
       ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 22, 105, 14),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: const Color(0xFF1E88E5),
+          side: const BorderSide(color: Color(0xFF1E88E5)),
+        ),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Color(0xFFFFC107),
+        foregroundColor: Colors.white,
+      ),
+      // cardTheme removed to avoid SDK-specific type mismatch
     );
   }
 }
